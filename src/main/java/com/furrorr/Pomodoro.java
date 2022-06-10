1ba8c434da81e45c5346e53c58058f166a8d9be7
furrorr <ageeff.ser220@yandex.ru>
2022-06-10 15:14:40 +0500

    pomodoro v3

--- src/main/java/com/furrorr/Pomodoro.java
+++ src/main/java/com/furrorr/Pomodoro.java
@@ -1,13 +1,11 @@
 package com.furrorr;
 
-import java.util.Arrays;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class Pomodoro {
 
     static boolean isTest = true;
-    static boolean isCallHelp = false;
     public static int test = 0;
 
     public static void main(String[] args) throws InterruptedException {
@@ -19,22 +17,22 @@ public class Pomodoro {
         int count = 1;
         int sizePrint = 60;
 
-        while(isTest) {
+        while (isTest) {
             System.out.print("Команда: ");
             String[] cmd = new Scanner(System.in).nextLine().split(" ");
             for (int i = 0; i < cmd.length; i++) {
                 switch (cmd[i]) {
                     case "-help" -> printHelpMessage();
                     case "-w" -> {
-                        if (i == cmd.length - 1) {  }
-                        else if (cmd[++i].equals("-b") || cmd[i].equals("-c")) {
+                        if (i == cmd.length - 1) {
+                        } else if (cmd[++i].equals("-b") || cmd[i].equals("-c")) {
                             --i;
                         } else workMinutes = Integer.parseInt(cmd[i]);
                         isTest = false;
                     }
                     case "-b" -> {
-                        if (i == cmd.length - 1) {  }
-                        else if (cmd[++i].equals("-c")) --i;
+                        if (i == cmd.length - 1) {
+                        } else if (cmd[++i].equals("-c")) --i;
                         else breakMinutes = Integer.parseInt(cmd[i]);
                         isTest = false;
                     }
@@ -50,25 +48,22 @@ public class Pomodoro {
                 }
             }
         }
-        if (!isCallHelp) {
-            if (workMinutes > 0 && breakMinutes > 0 && count > 0){
-                System.out.printf("Работаем %d min, отдыхаем %d min," +
-                        " повторям круг %d раз\n", workMinutes, breakMinutes, count);
-                long startTime = System.currentTimeMillis();
-                for (int i = 1; i <= count; i++) {
-                    timer(workMinutes, breakMinutes, sizePrint);
-                }
-                long endTime = System.currentTimeMillis();
-                System.out.println("Pomodoro таймер истек: " + (endTime - startTime) / (1000 * 60));
-            } else {
-                numberLessZero(workMinutes, breakMinutes);
-            }
-        }
+        if (workMinutes > 0 && breakMinutes > 0 && count > 0) startingTheTimer(workMinutes, breakMinutes, count, sizePrint);
+        else numberLessZero(workMinutes, breakMinutes);
+
         System.out.println("-------------Bye pomodoro!-------------");
     }
 
-
-
+    private static void startingTheTimer(int workMinutes, int breakMinutes, int count, int sizePrint) throws InterruptedException {
+        System.out.printf("Работаем %d min, отдыхаем %d min," +
+                " повторям круг %d раз\n", workMinutes, breakMinutes, count);
+        long startTime = System.currentTimeMillis();
+        for (int i = 1; i <= count; i++) {
+            timer(workMinutes, breakMinutes, sizePrint);
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("Pomodoro таймер истек: " + (endTime - startTime) / (1000 * 60));
+    }
 
     private static void numberLessZero(int workMinutes, int breakMinutes) {
         if (workMinutes < 0)
@@ -87,9 +82,9 @@ public class Pomodoro {
         int length;
         int rep;
         length = 60 * time / size;
-        rep = 60 * time /length;
-        double stretchb = size /(3.0 * time);
-        for(int i = 1; i <= rep; i++){
+        rep = 60 * time / length;
+        double stretchb = size / (3.0 * time);
+        for (int i = 1; i <= rep; i++) {
             double x = i;
             x = 1.0 / 3.0 * x;
             x *= 10;
@@ -103,8 +98,8 @@ public class Pomodoro {
             x /= 10;
             percent = Math.round(percent);
             percent /= 10;
-            System.out.print(process + percent+"% " + (" ").repeat(5 - (String.valueOf(percent).length())) +"[" + ("#").repeat(i) + ("-").repeat(rep - i)+"]    ( " + x +"min / " + time +"min )"+  "\r");
-            if(test == 0){
+            System.out.print(process + percent + "% " + (" ").repeat(5 - (String.valueOf(percent).length())) + "[" + ("#").repeat(i) + ("-").repeat(rep - i) + "]    ( " + x + "min / " + time + "min )" + "\r");
+            if (test == 0) {
                 TimeUnit.SECONDS.sleep(length);
             }
         }
@@ -114,36 +109,20 @@ public class Pomodoro {
     private static void printHelpMessage() {
         System.out.println("/*------------------------------------------------------------------------------------------------------------------------------*/");
 
-        String text = "\tПривет! Программа ставит таймер на [-w] минут для работы, [-b] минут для отдыха и на [-c] повторов.\n"+
-                "\t\tДефолтные значения: [-w] _ 25 минут\n"+
-                "\t\t\t\t\t\t\t[-b] _ 5 минут\n"+
-                "\t\t\t\t\t\t\t[-c] _ 1 повтор\n"+
-                "\tПримеры:\n"+
-                "\t\t~ Если вы хотите изменить все дефолтные значения, то введите:\n"+
-                "\t\t\t-w [количество_минут] -b [количество_минут] -с [количество_повторов] ___ -w 10 -b 5 -c 3 ___ -w 15 -b 10 -c 5 ___\n"+
-                "\t\t~ Если вы хотите изменить количество минут отдыха, но оставить без изменения количество минут работы и количество повторов, то введите:\n"+
-                "\t\t\t-b [количество_минут] ___ -b 10 ___ -b 7 ___ -b 20 ___\n"+
-                "\t\t~ Если вы хотите изменить количество минут работы и колчество повторов, но оставить без изменения количество минут отдыха, то введите:\n"+
-                "\t\t\t-w [количество_минут] -с [количество_повторов] ___ -w 10 -c 2 ___ -w 15 -c 3 ___ -w 5 -c 5 ___\n"+
+        String text = "\tПривет! Программа ставит таймер на [-w] минут для работы, [-b] минут для отдыха и на [-c] повторов.\n" +
+                "\t\tДефолтные значения: [-w] _ 25 минут\n" +
+                "\t\t\t\t\t\t\t[-b] _ 5 минут\n" +
+                "\t\t\t\t\t\t\t[-c] _ 1 повтор\n" +
+                "\tПримеры:\n" +
+                "\t\t~ Если вы хотите изменить все дефолтные значения, то введите:\n" +
+                "\t\t\t-w [количество_минут] -b [количество_минут] -с [количество_повторов] ___ -w 10 -b 5 -c 3 ___ -w 15 -b 10 -c 5 ___\n" +
+                "\t\t~ Если вы хотите изменить количество минут отдыха, но оставить без изменения количество минут работы и количество повторов, то введите:\n" +
+                "\t\t\t-b [количество_минут] ___ -b 10 ___ -b 7 ___ -b 20 ___\n" +
+                "\t\t~ Если вы хотите изменить количество минут работы и колчество повторов, но оставить без изменения количество минут отдыха, то введите:\n" +
+                "\t\t\t-w [количество_минут] -с [количество_повторов] ___ -w 10 -c 2 ___ -w 15 -c 3 ___ -w 5 -c 5 ___\n" +
                 "\t\t~ Для того, чтобы запустить программу с дефолтными значениями, просто нажмите Enter.\n";
         System.out.println(text);
 
-        System.out.println("/*------------------------------------------------------------------------------------------------------------------------------*/");        isCallHelp = true;
+        System.out.println("/*------------------------------------------------------------------------------------------------------------------------------*/");
     }
 }
-
-/*
-
-        Привет! Программа ставит таймер на [-w] минут для работы, [-b] минут для отдыха и на [-c] повторов.
-            Дефолтные значения: [-w] _ 25 минут
-                                [-b] _ 5 минут
-                                [-c] _ 1 повтор
-        Примеры:
-            ~ Если вы хотите изменить все дефолтные значения, то введите:
-                -w [количество_минут] -b [количество_минут] -с [количество_повторов] ___ -w 10 -b 5 -c 3 ___ -w 15 -b 10 -c 5 ___
-            ~ Если вы хотите изменить количество минут отдыха, но оставить без изменения количество минут работы и количество повторов, то введите:
-                -b [количество_минут] ___ -b 10 ___ -b 7 ___ -b 20 ___
-            ~ Если вы хотите изменить количество минут работы и колчество повторов, но оставить без изменения количество минут отдыха, то введите:
-                -w [количество_минут] -с [количество_повторов] ___ -w 10 -c 2 ___ -w 15 -c 3 ___ -w 5 -c 5 ___
-            ~ Для того, чтобы запустить программу с дефолтными значениями, просто нажмите Enter
- */
